d6f40d6076e5ad2907e25bda40cfe6b530519304
ghsr <ghsr92@yandex.ru>
2016-05-04 20:44:39 +0600

    i9082: Update widevine DRM blobs from sprout

--- i9082-vendor-blobs.mk
+++ i9082-vendor-blobs.mk
@@ -73,7 +73,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/i9082/proprietary/lib/libcontainers.so:system/lib/libcontainers.so \
     vendor/samsung/i9082/proprietary/lib/libdebug_sym.so:system/lib/libdebug_sym.so \
     vendor/samsung/i9082/proprietary/lib/libdispmanx.so:system/lib/libdispmanx.so \
-    vendor/samsung/i9082/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
     vendor/samsung/i9082/proprietary/lib/libhdcp.so:system/lib/libhdcp.so \
     vendor/samsung/i9082/proprietary/lib/libhdcp2.so:system/lib/libhdcp2.so \
     vendor/samsung/i9082/proprietary/lib/libHDCPKeyManager.so:system/lib/libHDCPKeyManager.so \
@@ -121,8 +120,8 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/i9082/proprietary/vendor/lib/hw/gralloc.capri.so:system/vendor/lib/hw/gralloc.capri.so \
     vendor/samsung/i9082/proprietary/vendor/lib/hw/hwcomposer.capri.so:system/vendor/lib/hw/hwcomposer.capri.so \
     vendor/samsung/i9082/proprietary/vendor/lib/hw/lights.capri.so:system/vendor/lib/hw/lights.capri.so \
+    vendor/samsung/i9082/proprietary/vendor/lib/libdrmmtkutil.so:system/vendor/lib/libdrmmtkutil.so \
     vendor/samsung/i9082/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     vendor/samsung/i9082/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
-    vendor/samsung/i9082/proprietary/vendor/lib/libWVphoneAPI.so:system/vendor/lib/libWVphoneAPI.so \
     vendor/samsung/i9082/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
     vendor/samsung/i9082/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
